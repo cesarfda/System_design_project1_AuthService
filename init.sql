ef3a0e8277d0c39b7c8d01faeda0133fa70cae37
Cesar Araujo <ara17008@byui.edu>
2023-01-01 14:28:44 -0800

    Baisc auth service and user Db

---
+++ init.sql
@@ -0,0 +1,15 @@
+CREATE USER 'authuser'@'localhost' IDENTIFIED BY 'auth123';
+
+CREATE DATABASE auth;
+
+GRANT ALL PRIVILEGES ON auth.* TO 'authuser'@'localhost';
+
+USE auth;
+
+CREATE TABLE user (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO user (email, password) VALUES ('cesar@email.com', '123456');
